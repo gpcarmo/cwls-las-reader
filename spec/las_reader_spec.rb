c33767f76e04aa6ce9f95b7e7bda8b71795f02c3
Gilberto P. Carmo Jr <gpcarmo@gmail.com>
2013-01-08 10:49:14 -0200

    New tests for LAS 2.0 format.

--- spec/las_reader_spec.rb
+++ spec/las_reader_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
   
-describe "CWLSLas LAS reader" do
+describe "CWLS LAS reader" do
 
   file_path = File.expand_path(File.dirname(__FILE__) + '/fixtures/files/')
   
@@ -9,20 +9,43 @@ describe "CWLSLas LAS reader" do
       las = CWLSLas.new
       it { expect { las.load_file('whatever.las') }.to raise_error("No such file or directory") } 
     end
+
+    context "when loading a LAS file version other then 1.2 or 2.0" do
+      las = CWLSLas.new
+      it { expect { las.load_file(file_path+'LAS_30a_Revised_2010.las') }.to raise_error("LAS version not supported") } 
+    end
   
-    context "when loading file: 'example1.las'" do
+    # LAS v1.2   
+    context "when loading LAS v1.2 file: 'example1.las'" do
         las = CWLSLas.new
         las.load_file(file_path+'/example1.las')
         it { las.should be_true }
     end
-    context "when loading file: 'example2.las'" do
+    context "when loading LAS v1.2 file: 'example2.las'" do
         las = CWLSLas.new
         las.load_file(file_path+'/example2.las')
         it { las.should be_true }
     end
-    context "when loading file: 'example3.las'" do
+    context "when loading LAS v1.2 file: 'example3.las'" do
         las = CWLSLas.new
         las.load_file(file_path+'/example3.las')
+        it { las.should be_true }
+    end
+    
+    # LAS v2.0   
+    context "when loading LAS v2.0 file: 'example21.las'" do
+        las = CWLSLas.new
+        las.load_file(file_path+'/example21.las')
+        it { las.should be_true }
+    end
+    context "when loading LAS v2.0 file: 'example22.las'" do
+        las = CWLSLas.new
+        las.load_file(file_path+'/example22.las')
+        it { las.should be_true }
+    end
+    context "when loading LAS v2.0 file: 'example23.las'" do
+        las = CWLSLas.new
+        las.load_file(file_path+'/example23.las')
         it { las.should be_true }
     end
   
@@ -47,19 +70,25 @@ describe "CWLSLas LAS reader" do
   end
 
   describe CWLSLas, "#curve" do
-    context "get depth curve from no_wrap mode file 'example1.las'" do
+    context "get depth curve from no_wrap mode LAS v1.2 file 'example1.las'" do
       c = [1670.0, 1669.875 , 1669.750]
       las = CWLSLas.new
       las.load_file(file_path+'/example1.las')
       it { expect(las.curve('DEPT').log_data).to eq(c) }
     end
-    context "get depth curve from wrap_mode file 'example3.las'" do
+    context "get depth curve from wrap_mode LAS v1.2 file 'example3.las'" do
       c = [910.0, 909.875, 909.75, 909.625, 909.5]
       las = CWLSLas.new
       las.load_file(file_path+'/example3.las')
       it { expect(las.curve('DEPT').log_data).to eq(c) }
     end
-    context "get DT curve from wrap_mode with nil values from 'example3.las'" do
+    context "get DT curve from wrap_mode with nil values from LAS v1.2 file 'example3.las'" do
+      c = [nil, nil, nil, nil, nil]
+      las = CWLSLas.new
+      las.load_file(file_path+'/example3.las')
+      it { expect(las.curve('DT').log_data).to eq(c) }
+    end
+    context "get DT curve from wrap_mode with nil values from LAS v2.0 file 'example23.las'" do
       c = [nil, nil, nil, nil, nil]
       las = CWLSLas.new
       las.load_file(file_path+'/example3.las')
@@ -68,12 +97,18 @@ describe "CWLSLas LAS reader" do
   end
 
   describe CWLSLas, "#well_name" do
-    context "get well name from las file 'example1.las'" do
+    context "get well name from las v1.2 file 'example1.las'" do
       well_name = "ANY ET AL OIL WELL #12"
       las = CWLSLas.new
       las.load_file(file_path+'/example1.las')
       it { expect(las.well_name).to eq(well_name) }
     end
+    context "get well name from las v2.0 file 'example21.las'" do
+      well_name = "ANY ET AL 12-34-12-34"
+      las = CWLSLas.new
+      las.load_file(file_path+'/example21.las')
+      it { expect(las.well_name).to eq(well_name) }
+    end
   end
-  
+
 end
